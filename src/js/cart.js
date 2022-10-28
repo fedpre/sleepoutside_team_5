ee9a07551dbd2d4a790acc9d354e5e17598bdc44
sh1eldma1den <@pOOsa856100>
2022-10-28 09:42:41 -0400

    first commit

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,7 +3,6 @@ function getLocalStorage(key) {
 }
 
 function getCartContents() {
-  let markup = '';
   const cartItems = getLocalStorage('so-cart');
   const htmlItems = cartItems.map((item) => renderCartItem(item));
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
@@ -11,7 +10,7 @@ function getCartContents() {
 }
 
 function renderCartItem(item) {
-const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -25,7 +24,6 @@ const newItem = `<li class="cart-card divider">
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-  console.log(newItem);
   return newItem;
 }
 
