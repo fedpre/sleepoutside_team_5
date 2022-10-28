3bd0cd5795ad82e6e775c60410d62819dc1d13f4
fedpre <fed.pregnolato@gmail.com>
2022-10-28 18:42:49 +0200

    fixed broken cart and Talus tent code in html

--- src/js/product.js
+++ src/js/product.js
@@ -1,23 +1,27 @@
-let products = [];
+let products = []
 function convertToJson(res) {
   if (res.ok) {
-    return res.json();
+    return res.json()
   } else {
-    throw new Error("Bad Response");
+    throw new Error('Bad Response')
   }
 }
 
 function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
+  localStorage.setItem(key, JSON.stringify(data))
+}
+
+function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key))
 }
 
 // get tents data
 function getProductsData() {
-  fetch("../json/tents.json")
+  fetch('../json/tents.json')
     .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
+    .then(data => {
+      products = data
+    })
 }
 // or should we do it this way?
 // async function getProductsDataAwait() {
@@ -26,11 +30,18 @@ function getProductsData() {
 
 // add to cart button event handler
 function addToCart(e) {
-  console.log("here");
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  setLocalStorage("so-cart", product);
+  // Find the product selected among all the products
+  const product = products.find(item => item.Id === e.target.dataset.id)
+  // Get the current items stores in localStorage
+  const localItems = getLocalStorage('so-cart')
+  // Check if there are already items there and add the new item
+  if (localItems === null) {
+    setLocalStorage('so-cart', [product])
+  } else {
+    setLocalStorage('so-cart', [...localItems, product])
+  }
 }
 
-getProductsData();
+getProductsData()
 // add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
+document.getElementById('addToCart').addEventListener('click', addToCart)
