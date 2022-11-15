b85f3a967a74f214cb41c2af80d13714c27777e4
fedpre <fed.pregnolato@gmail.com>
2022-11-15 12:13:53 +0100

    Stretch completed --bug button not working

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,81 +1,25 @@
-import { getLocalStorage, loadHeaderFooter, setLocalStorage } from './utils'
+import ShoppingCart from './shoppingCart'
+import { getLocalStorage, loadHeaderFooter } from './utils'
 
-function getCartContents() {
-  const cartItem = getLocalStorage('so-cart')
-  if (cartItem == null) {
-    return
-  }
-  const renderItems = cartItem.map(item => renderCartItem(item))
+const parentNode = document.querySelector('.product-list')
+const selector = '#product-cart-template'
+const shoppingCart = new ShoppingCart(parentNode)
 
-  document.querySelector('.product-list').innerHTML = renderItems.join('')
-}
+shoppingCart.init(selector)
 
-function renderCartItem(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-   <a href="" class="cart-card__delete "><span class="material-symbols-outlined" data-id=${item.Id}>delete</span></a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: <span class="qt-num">${item.quantity}</span></p>
-<button data-id=${item.Id} class="cart-card__addQuantity btn-secondary">+</button>
-<button data-id=${item.Id} class="cart-card__removeQuantity btn-secondary">-</button>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`
-  return newItem
-}
-
-function removeItem(e) {
-  e.preventDefault()
-  const id = e.target.dataset.id
-  if (getLocalStorage('so-cart') !== null) {
-    const items = getLocalStorage('so-cart')
-    const newItems = items.filter(item => item.Id !== id)
-    setLocalStorage('so-cart', newItems)
-    window.location.reload()
-  }
-}
-
-function addQuantity(e) {
-  const id = e.target.dataset.id
-  const currItems = getLocalStorage('so-cart')
-  const addItemsArray = currItems.map(item =>
-    item.Id === id ? { ...item, quantity: item.quantity + 1 } : item
-  )
-  setLocalStorage('so-cart', addItemsArray)
-  window.location.reload()
-}
-
-function removeQuantity(e) {
-  const id = e.target.dataset.id
-  const currItems = getLocalStorage('so-cart')
-  const addItemsArray = currItems.map(item =>
-    item.Id === id ? { ...item, quantity: item.quantity - 1 } : item
-  )
-  setLocalStorage('so-cart', addItemsArray)
-  window.location.reload()
-}
-
-getCartContents()
 const cartItems = getLocalStorage('so-cart')
 if (cartItems !== null) {
   const deleteListeners = document.querySelectorAll('.cart-card__delete')
   const deleteListenersArray = Array.from(deleteListeners)
   deleteListenersArray.map(listener =>
-    listener.addEventListener('click', removeItem)
+    listener.addEventListener('click', shoppingCart.removeItem)
   )
 
   // Listeners for add buttons
   const addListeners = document.querySelectorAll('.cart-card__addQuantity')
   const addListenersArray = Array.from(addListeners)
   addListenersArray.map(listener =>
-    listener.addEventListener('click', addQuantity)
+    listener.addEventListener('click', shoppingCart.addQuantity)
   )
 
   // Listeners for remove buttons
@@ -84,7 +28,7 @@ if (cartItems !== null) {
   )
   const removeListenersArray = Array.from(removeListeners)
   removeListenersArray.map(listener =>
-    listener.addEventListener('click', removeQuantity)
+    listener.addEventListener('click', shoppingCart.removeQuantity)
   )
 }
 
---
+++ src/js/shoppingCart.js
@@ -0,0 +1,93 @@
+import {
+  getLocalStorage,
+  setLocalStorage,
+  renderListWithTemplate
+} from './utils'
+
+export default class ShoppingCart {
+  constructor(listElement) {
+    this.cartList = []
+    this.listElement = listElement
+  }
+
+  async init(selector) {
+    this.cartList = await this.getCartContent()
+    this.renderCartItems(this.cartList, selector, this.listElement)
+  }
+
+  getCartContent() {
+    const cartList = getLocalStorage('so-cart')
+    if (cartList == null) {
+      return []
+    }
+    return cartList
+  }
+
+  prepareTemplate(node, item) {
+    const finalNode = node
+
+    const itemImg = finalNode.querySelector('.prod-img')
+    itemImg.src = item.Image
+    item.alt = item.Name
+    const titleName = finalNode.querySelector('.card__name')
+    titleName.innerHTML = item.Name
+    const deleteBtn = finalNode.querySelector('.material-symbols-outlined')
+    deleteBtn.setAttribute('data-id', item.Id)
+    const color = finalNode.querySelector('.cart-card__color')
+    color.innerHTML = item.Colors[0].ColorName
+    const quantity = finalNode.querySelector('.qt-num')
+    quantity.innerHTML = item.quantity
+    const addQuantityBtn = finalNode.querySelector('.cart-card__addQuantity')
+    addQuantityBtn.setAttribute('data-id', item.Id)
+    const removeQuantityBtn = finalNode.querySelector(
+      '.cart-card__removeQuantity'
+    )
+    removeQuantityBtn.setAttribute('data-id', item.Id)
+    const finalPrice = finalNode.querySelector('.cart-card__price')
+    finalPrice.innerHTML = item.FinalPrice
+
+    return finalNode
+  }
+
+  renderCartItems(itemList, selector, parentNode) {
+    const templateElement = document.querySelector(selector)
+    console.log(templateElement)
+    renderListWithTemplate(
+      templateElement,
+      parentNode,
+      itemList,
+      this.prepareTemplate
+    )
+  }
+
+  removeItem(e) {
+    e.preventDefault()
+    const id = e.target.dataset.id
+    if (getLocalStorage('so-cart') !== null) {
+      const items = getLocalStorage('so-cart')
+      const newItems = items.filter(item => item.Id !== id)
+      setLocalStorage('so-cart', newItems)
+      window.location.reload()
+    }
+  }
+
+  addQuantity(e) {
+    const id = e.target.dataset.id
+    const currItems = getLocalStorage('so-cart')
+    const addItemsArray = currItems.map(item =>
+      item.Id === id ? { ...item, quantity: item.quantity + 1 } : item
+    )
+    setLocalStorage('so-cart', addItemsArray)
+    window.location.reload()
+  }
+
+  removeQuantity(e) {
+    const id = e.target.dataset.id
+    const currItems = getLocalStorage('so-cart')
+    const addItemsArray = currItems.map(item =>
+      item.Id === id ? { ...item, quantity: item.quantity - 1 } : item
+    )
+    setLocalStorage('so-cart', addItemsArray)
+    window.location.reload()
+  }
+}
