7fa38a17edb8a991aed250e549de3bf1895c5011
fedpre <fed.pregnolato@gmail.com>
2022-12-07 19:44:14 +0100

    added color choice detail page

--- src/js/product.js
+++ src/js/product.js
@@ -2,18 +2,21 @@ import ExternalServices from './ExternalServices'
 import { getParams } from './utils'
 import ProductDetails from './productDetails'
 
-const productId = getParams('product')
-const category = getParams('category')
-const externalServices = new ExternalServices()
-const parentNodeBreadcrumb = document.querySelector('.breadcrumb')
-const product = new ProductDetails(productId, externalServices, category)
-product.init().then(() => {
-  ;(function createBreadcrumb() {
-    const prodCatNode = document.createElement('span')
-    prodCatNode.innerText = `${category} >> `
-    parentNodeBreadcrumb.appendChild(prodCatNode)
-    const itemName = document.createElement('span')
-    itemName.innerText = product.getName()
-    parentNodeBreadcrumb.appendChild(itemName)
-  })()
-})
+(function main() {
+  const productId = getParams('product')
+  const category = getParams('category')
+  const externalServices = new ExternalServices()
+  const parentNodeBreadcrumb = document.querySelector('.breadcrumb')
+  const product = new ProductDetails(productId, externalServices, category)
+  product.init().then(() => {
+    (function createBreadcrumb() {
+      const prodCatNode = document.createElement('span')
+      prodCatNode.innerText = `${category} >> `
+      parentNodeBreadcrumb.appendChild(prodCatNode)
+      const itemName = document.createElement('span')
+      itemName.innerText = product.getName()
+      parentNodeBreadcrumb.appendChild(itemName)
+    })()
+  })
+})()
+
